308364903f36ac697ce0de8384768c13ca8d0da3
Santiago Argañaraz <santiagojoel099@gmail>
2024-04-19 12:09:39 -0300

    Cambios en la logica de registro

--- src/views/RegisterModal.jsx
+++ src/views/RegisterModal.jsx
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Form, FormControl, FormGroup, Modal } from "react-bootstrap";
+import {addUser} from "../utils"
 
 const RegisterModal = ({ show, handleClose }) => {
-  const handleRegister = () => {
-    // Lógica para registrar al usuario
-    handleClose();
+  const [nombre, setNombre] = useState("");
+  const [email, setEmail] = useState("");
+  const [contraseña, setContraseña] = useState("");
+
+  const handleRegister = async () => {
+    const usuario = {
+      name: nombre,
+      mail: email,
+      password: contraseña
+    };
+
+    const registrado = await addUser(usuario);
+
+    if (registrado) {
+      
+      handleClose();
+    } else {
+      
+      alert("Error al registrar usuario");
+    }
   };
 
   return (
@@ -15,17 +33,38 @@ const RegisterModal = ({ show, handleClose }) => {
       <Modal.Body>
         <Form>
           <FormGroup>
+            <Form.Label>Nombre</Form.Label>
+            <FormControl
+              type="text"
+              placeholder="Ingrese su nombre completo"
+              value={nombre}
+              onChange={(e) => setNombre(e.target.value)}
+            />
+          </FormGroup>
+          <FormGroup>
             <Form.Label>Email</Form.Label>
-            <FormControl type="email" placeholder="Ingrese su correo electrónico" />
+            <FormControl
+              type="email"
+              placeholder="Ingrese su correo electrónico"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </FormGroup>
           <FormGroup>
             <Form.Label>Contraseña</Form.Label>
-            <FormControl type="password" placeholder="Ingrese su contraseña" />
+            <FormControl
+              type="password"
+              placeholder="Ingrese su contraseña"
+              value={contraseña}
+              onChange={(e) => setContraseña(e.target.value)}
+            />
           </FormGroup>
         </Form>
       </Modal.Body>
       <Modal.Footer>
-        <a href="/" className="btn btn-secondary">Iniciar Sesión</a>
+        <a href="/" className="btn btn-secondary">
+          Iniciar Sesión
+        </a>
         <Button variant="primary" onClick={handleRegister}>
           Registrarse
         </Button>
